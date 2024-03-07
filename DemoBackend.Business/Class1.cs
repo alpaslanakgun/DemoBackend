2055b77e9b65635df36f9212def8df76e0b2d366
Alpaslan Akgün <alpaslan.akgun@outlook.com>
2024-03-07 21:53:17 +0300

    Add project files.

---
+++ DemoBackend.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DemoBackend.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ DemoBackend.Entities/Concrete/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoBackend.Entities.Concrete
+{
+    public  class Product
+    {
+        public int ProductID { get; set; }
+        public string  ProductName { get; set; }
+        public int  CategoryID { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal  UnitPrice  { get; set; }
+        public short UnitsInStock { get; set; }
+    }
+}
